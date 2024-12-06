c90bbfed46d50088a9ac833446d4c406962dfb94
Vagarth <vagarth419@gmail.com>
2024-12-06 23:47:46 +0530

    feat: solve palindrome assignment, all test cases passing

--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -4,7 +4,14 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  let str1 = str.toLowerCase()
+  let string1 = str1.replace(/[&\/\\#, +()$~%.'":*?!<>{}]/g, '')
+  let string2 = string1.split('').reverse().join('');
+  if (string1 === string2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isPalindrome;
