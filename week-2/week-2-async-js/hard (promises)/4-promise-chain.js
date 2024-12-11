e96d298c1f16c0a0d93ca95dfcda797b67f46287
Vagarth <vagarth419@gmail.com>
2024-12-11 18:31:03 +0530

    feat: solve promise chain assignment, all test cases passing

--- week-2/week-2-async-js/hard (promises)/4-promise-chain.js
+++ week-2/week-2-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,36 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, t * 1000);
+    })
 }
 
 function wait2(t) {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, t * 1000);
+    })
 }
 
 function wait3(t) {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, t * 1000);
+    })
 }
 
-function calculateTime(t1, t2, t3) {
+const sequence = (t1, t2, t3) => {
+    return wait1(t1).then(() => {
+        return wait2(t2).then(() => {
+            return wait3(t3)
+        })
+    })
+}
 
+async function calculateTime(t1, t2, t3) {
+    let startTime = Date.now();
+    await sequence(t1, t2, t3)
+    let endTime = Date.now() - startTime;
+    return endTime;
 }
 
 module.exports = calculateTime;
